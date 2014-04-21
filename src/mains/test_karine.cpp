5d7500a634bad5645579304b89b4787e0a82c5a6
David San <davidsanfr@gmail.com>
2014-04-21 16:53:19 +0200

    restore original test_karine main

--- src/mains/test_karine.cpp
+++ src/mains/test_karine.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <OPLabel.h>
 #include <Program.h>
 #include <OPExpression.h>
@@ -6,12 +6,12 @@
 #include <OPImmediate.h>
 #include <Cfg.h>
 
-/*
- parsing d'un fichier assembleur
- affichage du programme
- calcul des fonction, affichage du nombre de fonction
- EXEMPLE DE TEST des méthodes que vous devez coder
- */
+/* 
+parsing d'un fichier assembleur 
+affichage du programme
+calcul des fonction, affichage du nombre de fonction
+EXEMPLE DE TEST des méthodes que vous devez coder
+*/
 
 int main(int argc, char ** argv){
   if (argc < 2){
@@ -19,53 +19,46 @@ int main(int argc, char ** argv){
     return -1;
   }
   Program p2(argv[1]) ;
-  //  p2.display() ;
-  
-  
-  
+  p2.display() ;
+
+ 
+
   /* Test DU CALCUL DES FONCTIONS */
-  /* Calcul des fonctions contenues dans le fichier parsé */
+   /* Calcul des fonctions contenues dans le fichier parsé */
+ 
+  /*
   p2.comput_function();
   cout << "dans le fichier " << argv[1]  << " nb function : " << p2.nbr_func() << endl;
-  
+  */
+
   /* TEST DU CALCUL DES BB d'une fonction */
-  /* Pour tester le calcul des BB d'une fonction */
-  // Function * fct = p2.get_function(0);
-  // fct -> comput_basic_block();
-  
-  
-  /* EXEMPLE DE TEST */
-  /* Pour tester le calcul des blocs de bases puis du calcul des BB pred/succ des BB */
-  
-  
-  
-  
-  Function * fct = p2.get_function(0);
-  fct -> comput_basic_block();
-  fct -> comput_label();
-  fct -> comput_succ_pred_BB();
-  // fct -> test();
+  /* Pour tester le calcul des BB d'une fonction */ 
+  /* Function * fct = p2.get_function(0);
+     fct -> comput_basic_block(); 
 
+  */
 
-  fct->get_BB(0)->display();
-
-
-  cerr << "Renaming" << endl;
-
-  list<int> l;
-  fct->get_BB(0)->register_rename(l);
+                /* EXEMPLE DE TEST */
+  /* Pour tester le calcul des blocs de bases puis du calcul des BB pred/succ des BB */ 
   
-  cerr << "After renaming" << endl;
 
-  fct->get_BB(0)->display();
+  /* 
+
+     Function * fct = p2.get_function(0);
+     fct -> comput_basic_block(); 
+     fct -> comput_succ_pred();
+     fct -> test(); 
+
+  */
+
 
   /* TEST DE LA CONSTRUCTION DES CFG et donc des succ/pred des BB */
-//  if (p2.nbr_func() > 2){
-//    p2.get_function(2)->display();
-//    p2.comput_CFG();
-//    Cfg * cfg = p2.get_CFG(p2.nbr_func()>2?2:0);
-//    cfg -> restitution(NULL,"./tmp/graph2.dot");
-//  }
-
+  /*  if (p2.nbr_func() > 0){
+    p2.get_function(0)->display();
+    p2.comput_CFG();
+    Cfg * cfg = p2.get_CFG(p2.nbr_func()>2?2:0);
+    cfg -> restitution(NULL,"./tmp/graph2.dot");
+  }
+  */
   return 0;
 }
